cc83ee2bbeba0bdaae5deab695af031d97ca520c
RenXenpai11 <laurencejan1431@gmail.com>
2026-06-25 22:15:12 +0800

    Extend PlayerHUD with Mana fill/label logic

--- Assets/Scripts/UI/PlayerHUD.cs
+++ Assets/Scripts/UI/PlayerHUD.cs
@@ -10,17 +10,26 @@ namespace TheLastAethon.UI
         [SerializeField] private PlayerHealth playerHealth;
         [SerializeField] private Image hpFillImage;
         [SerializeField] private TextMeshProUGUI hpLabel;
+        [SerializeField] private PlayerMana playerMana;
+        [SerializeField] private Image manaFillImage;
+        [SerializeField] private TextMeshProUGUI manaLabel;
 
         private static readonly Color HighColor = new Color(0.7529412f, 0.2235294f, 0.1686275f);
         private static readonly Color MidColor = new Color(0.9019608f, 0.4941176f, 0.1333333f);
         private static readonly Color LowColor = new Color(0.9058824f, 0.2980392f, 0.2352941f);
+        private static readonly Color ManaColor = new Color(0.2039216f, 0.5960784f, 0.8588235f);
 
         private void Update()
         {
-            float pct = (float)playerHealth.Hp / playerHealth.MaxHp;
-            hpFillImage.fillAmount = pct;
-            hpFillImage.color = pct > 0.5f ? HighColor : pct > 0.25f ? MidColor : LowColor;
+            float hpPct = (float)playerHealth.Hp / playerHealth.MaxHp;
+            hpFillImage.fillAmount = hpPct;
+            hpFillImage.color = hpPct > 0.5f ? HighColor : hpPct > 0.25f ? MidColor : LowColor;
             hpLabel.text = $"{playerHealth.Hp} / {playerHealth.MaxHp}";
+
+            float manaPct = (float)playerMana.Mana / playerMana.MaxMana;
+            manaFillImage.fillAmount = manaPct;
+            manaFillImage.color = ManaColor;
+            manaLabel.text = $"{playerMana.Mana} / {playerMana.MaxMana}";
         }
     }
 }
